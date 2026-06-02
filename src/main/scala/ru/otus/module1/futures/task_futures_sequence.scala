0240122e7dd00d6b5fdc7be87f357316f7cf92d9
Stanislav <stas@example.com>
2026-06-02 21:58:01 +0300

    add homework for 08 module

--- src/main/scala/ru/otus/module1/futures/task_futures_sequence.scala
+++ src/main/scala/ru/otus/module1/futures/task_futures_sequence.scala
@@ -2,7 +2,10 @@ package ru.otus.module1.futures
 
 import ru.otus.module1.futures.HomeworksUtils.task
 
+import scala.concurrent
+import scala.concurrent.impl.Promise
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Failure, Success, Try}
 
 object task_futures_sequence {
 
@@ -19,8 +22,52 @@ object task_futures_sequence {
    * @param futures список асинхронных задач
    * @return асинхронную задачу с кортежом из двух списков
    */
+
+  // жрет лимиты нещадно
+  def fullSequence1[A](futures: List[Future[A]])
+                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
+
+    val fs: Future[List[Try[A]]] = Future.sequence(
+    futures
+      .map(f => f.transform(Try(_)))
+    )
+
+    val suc: Future[List[A]] = fs.map(_.collect{case Success(v) => v})
+    val fail: Future[List[Throwable]] = fs.map(_.collect{case Failure(ex) => ex})
+
+    suc.zip(fail)
+  }
+
+  // это лучше, но не идеально, надо как можно меньше запуска фьюч
   def fullSequence[A](futures: List[Future[A]])
-                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] =
-    task"Реализуйте метод `fullSequence`" ()
+                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
+
+    futures.foldRight(Future.successful((List[A](), List[Throwable]()))) { (f, acc) =>
+      f.flatMap {
+        v => acc.map((lf, lt) => (v +: lf, lt))
+      }
+        .recoverWith {
+          case t: Throwable => acc.map((lf, lt) => (lf, t +: lt))
+      }
+    }
+  }
+
+
+  // тоже пожирает токены
+  def fullSequence2[A](futures: List[Future[A]])
+                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
+
+    val fs: Future[List[A]] = Future.sequence(
+      futures
+    )
+
+    fs.map{list =>
+      list.foldRight((List[A](), List[Throwable]())){
+        case (v: A, (lf, lt)) => (v +: lf, lt)
+        case (e: Throwable, (lf, lt)) => (lf, e +: lt)
+      }
+    }
+
+    }
 
 }
