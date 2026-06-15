4f61a5515690c28ac5a3805bd09d1754b2738bff
Stanislav <stas@example.com>
2026-06-15 13:14:29 +0300

    переводим на scala 3

--- src/main/scala/ru/otus/module2/10-typeClassesHW.scala
+++ src/main/scala/ru/otus/module2/10-typeClassesHW.scala
@@ -1,46 +1,38 @@
 package ru.otus.module2
 
-import ru.otus.module2.type_classes_hw.JsValue.{
-  JsNull,
-  JsNumber,
-  JsObject,
-  JsString
-}
+import ru.otus.module2.type_classes_hw.JsValue.{JsNull, JsNumber, JsObject, JsString}
+import ru.otus.module2.type_classes_hw.{JsValue, JsonWriter}
 import ru.otus.module2.type_classes_hw.JsonWriter.given
 
 
 object type_classes_hw {
 
-  sealed trait JsValue
+  // убрали скобки, вынесли toJson вне объекта потому что можем
+  // выкинуть object type_classes_hw скорее всего тоже можем, но будут конфликты при импорте
+  //в остальном вроде все вписывается вписывается в scala 3
 
-  object JsValue {
-    final case class JsObject(get: Map[String, JsValue]) extends JsValue  {
-      override def toString: String = this.get.mkString("{\n", ", \n", "\n}")
+  // меняем силд иерхию на enum
+  enum JsValue:
+    case JsObject(get: Map[String, JsValue])
+    case JsString(get: String)
+    case JsNumber(get: Double)
+    case JsNull
+
+    override def toString: String = this match {
+      case JsValue.JsObject(get) => get.mkString("{\n", ", \n", "\n}")
+      case JsValue.JsString(get) => s""""$get""""
+      case JsValue.JsNumber(get) => s"$get"
+      case JsValue.JsNull => "null"
     }
-
-    final case class JsString(get: String) extends JsValue {
-      override def toString: String = s""""${this.get}""""
-    }
-
-
-    final case class JsNumber(get: Double) extends JsValue {
-      override def toString: String = s"""${this.get}"""
-    }
-
-    case object JsNull extends JsValue {
-        override def toString: String = """null"""
-    }
-  }
   
-  // 1
-  
-  trait JsonWriter[T] {
+  trait JsonWriter[T]:
     def toJson(v: T): JsValue
 
+    // по сути одно и тоже, просто по мне object.toJson красивее toJson(object)
     extension (v: T) def toJsonIm: JsValue
-  }
+
   
-  object JsonWriter {
+  object JsonWriter:
     
     def apply[T](using ev: JsonWriter[T]) = ev
     
@@ -54,24 +46,25 @@ object type_classes_hw {
 
     given JsonWriter[Int] = from[Int](JsNumber)
 
+    // жуткий костыль, чтобы в мапе могли быть разные значения, ну естественно для которых есть JsonWriter
+    given JsonWriter[Any] = from[Any] {
+      case s: String => JsString(s)
+      case n: Int => JsNumber(n)
+      case opt: Option[Any] => toJson(opt)
+      case _ => JsString("Not implemented")
+    }
+
 
     given optJson [T](using jw: JsonWriter[T]): JsonWriter[Option[T]] = from[Option[T]] {
       case Some(value) => jw.toJson(value)
       case None => JsNull
     }
 
-    given [T](using  jw: JsonWriter[T]): JsonWriter[Map[String, T]] =  from[Map[String, T]](
+    given [T]: JsonWriter[Map[String, T]] =  from[Map[String, T]](
       m => JsObject(
         m.map {(k, v) => (k -> v.toJsonIm)}
       )
     )
-
-  }
-
-
-  def toJson[T: JsonWriter](v: T): JsValue = JsonWriter[T].toJson(v)
-  
-
 
   @main
   def hw10(): Unit = {
@@ -79,11 +72,18 @@ object type_classes_hw {
     println("vffv".toJsonIm)
     println(toJson(10))
     println(toJson(Option(10)))
-    println(toJson(Option(List("").tail.headOption)))
-    println(toJson(Map("one" -> Option("1"), "two" -> Option("2"))))
+    println(toJson(Option(List("").tail.headOption))) // идея не дает подсунуть просто None
+    println(toJson(Map[String, Any](
+      "one" -> 1,
+      "two" -> "2",
+      "opt" -> Option(22),
+      "noImpl" -> 22.3
+    )))
   }
 }
 
+// может существовать без пакеджа и не завернут в другую структуру
+def toJson[T: JsonWriter](v: T): JsValue = JsonWriter[T].toJson(v)
 
 
 
